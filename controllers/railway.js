fc83401dd694a871227a0280c46a10d4bdb9ca74
VenkatGadde9999 <venkat.gadde234@gmail.com>
2021-04-14 21:10:12 -0500

    assignment

--- controllers/railway.js
+++ controllers/railway.js
@@ -48,9 +48,23 @@ exports.railway_delete = function(req, res) {
  res.send('NOT IMPLEMENTED: railway delete DELETE ' + req.params.id);
 };
 // Handle railway update form on PUT.
-exports.railway_update_put = function(req, res) {
- res.send('NOT IMPLEMENTED: railway update PUT' + req.params.id);
+exports.railway_update_put = async function(req, res) {
+    console.log(`update on id ${req.params.id} with body ${JSON.stringify(req.body)}`)
+    try {
+        let toUpdate = await railway.findById( req.params.id)
+        // Do updates of properties
+        if(req.body.city) toUpdate.city = req.body.city;
+        if(req.body.destination) toUpdate.destination = req.body.destination;
+        if(req.body.cost) toUpdate.cost = req.body.cost;
+        let result = await toUpdate.save();
+        console.log("Sucess " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": ${err}: Update for id ${req.params.id} failed`);
+    }
 };
+
 // VIEWS
 // Handle a show all view
 exports.railway_view_all_Page = async function(req, res) {
@@ -62,4 +76,71 @@ exports.railway_view_all_Page = async function(req, res) {
     res.status(500);
     res.send(`{"error": ${err}}`);
     }
-   };
+   };   
+
+   
+// Handle a show one view with id specified by query
+exports.railway_view_one_Page = async function (req, res) {
+    console.log("single view for id " + req.query.id)
+    try {
+        result = await railway.findById(req.query.id)
+        res.render('railwaydetail', {
+            title: 'railway Detail',
+            toShow: result
+        });
+    } catch (err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+// Handle building the view for creating a railway.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.railway_create_Page =  function(req, res) {
+    console.log("create view")
+    try{
+        res.render('railwaycreate', { title: 'Create railway'});
+    }
+    catch(err){
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+// Handle building the view for updating a railway.
+// query provides the id
+exports.railway_update_Page =  async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+        let result = await railway.findById(req.query.id)
+        res.render('railwayupdate', { title: 'Update railway Details', toShow: result });
+    }
+    catch(err){
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+// Handle railway delete on DELETE.
+exports.railway_delete = async function(req, res) {
+    console.log("delete "  + req.params.id)
+    try {
+        result = await railway.findByIdAndDelete(req.params.id)
+        console.log("Removed " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": Error deleting ${err}}`);
+    }
+};
+
+// Handle a delete one view with id from query
+exports.railway_delete_Page = async function(req, res) {
+    console.log("Delete view for id "  + req.query.id)
+    try{
+        result = await railway.findById(req.query.id)
+        res.render('railwaydelete', { title: 'railway Delete', toShow: result });
+    }
+    catch(err){
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
--- routes/railway.js
+++ routes/railway.js
@@ -4,6 +4,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', railway_controlers.railway_view_all_Page );
-
+/* GET detail railway page */
+router.get('/detail', railway_controlers.railway_view_one_Page);
+/* GET create railway page */
+router.get('/create', railway_controlers.railway_create_Page);
+/* GET create update page */
+router.get('/update', railway_controlers.railway_update_Page);
+/* GET create railway page */
+router.get('/delete', railway_controlers.railway_delete_Page)
 
 module.exports = router;
